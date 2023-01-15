91bc47bd102ada1e8cc9f43c50918a783cad17b5
DoaaMahmoud2020 <doaaberam@yahoo.com>
2023-01-15 18:27:45 +0200

    Upload All Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './theme/header/header.component';
+import { FooterComponent } from './theme/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SwiperModule } from 'swiper/angular';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SwiperModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
